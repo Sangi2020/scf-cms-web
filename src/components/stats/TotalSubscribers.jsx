9895061a0ea8dc2fdbad1b1e1bbd0c2e990d8a39
guru-das-pr <gurudaspr.tltechnologies@gmail.com>
2024-11-21 10:26:37 +0530

    added midudev animation

--- src/components/stats/TotalSubscribers.jsx
+++ src/components/stats/TotalSubscribers.jsx
@@ -25,7 +25,7 @@ const TotalSubscribers = () => {
     }, []);
 
     return (
-        <div className="w-full">
+        <div className="w-full animate-fade-in-down">
             {loading ? (
                 <SkeletonCard />
             ) : (
