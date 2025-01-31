f53ba2785cc5cb8cd7af3b4cdb857f8087cc83b4
arjunTltech <arjunntltechnologies@gmail.com>
2025-01-31 19:43:34 +0530

    updated error pages

--- src/pages/error/Error500.jsx
+++ src/pages/error/Error500.jsx
@@ -1,24 +1,34 @@
+import { ArrowBigRight, ArrowRight } from 'lucide-react';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Error500 = () => {
-  const navigate = useNavigate(); // Hook to navigate programmatically
+  const navigate = useNavigate(); 
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-base-200 text-base-content">
-      <div className="text-center">
-        <h1 className="text-9xl font-bold text-error">500</h1>
-        <h2 className="text-3xl font-semibold mt-4">Internal Server Error</h2>
-        <p className="mt-2 text-lg">
-          Something went wrong on our end. Please try again later.
-        </p>
-        <button
-          className="btn btn-primary mt-6"
-          onClick={() => navigate('/')} // Navigate to the home page
-        >
-          Go Back Home
-        </button>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-white">
+      <div className="mb-8">
+        <img
+          src="/images/errors/error_500.jpg"
+          alt="500 Error Illustration"
+          className="max-w-[400px] w-full"
+        />
       </div>
+
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-gray-800  mb-3">500 - Internal Server Error</h1>
+        <p className="text-xl text-gray-400 mb-4">
+          Oops! Something went wrong on our end. Don't worry, our team is already on it.
+        </p>
+      </div>
+
+        <button 
+        onClick={() => navigate("/")}
+        className="px-6 py-3 bg-white text-red-500 font-medium shadow-sm flex outline-red-400 
+                         rounded-full  transition-all duration-300">
+          Go to Homepage&nbsp;
+          <ArrowRight size={20} className='my-auto'/>
+        </button>
     </div>
   );
 };
